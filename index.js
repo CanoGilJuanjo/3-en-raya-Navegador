052d7c855d498d7aa3e7248aa0aab19e819748fc
JuanjoCanoGil <jcg0039@alu.medac.es>
2024-01-11 13:11:28 +0100

    IA coloca siempre y se defiende siempre

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function turnoIA(i,j){
 
     //En caso de poder hacer 3 en raya la IA ataca
     if(turno){
+
         //Horizontal
         for(let i = 0; i<DIMENSION_MATRIZ && turno; i++){
             let oCount = 0;      
@@ -45,6 +46,7 @@ function turnoIA(i,j){
                 turno = false;
             }
         }
+
         //Vertical
         for(let j = 0; j<DIMENSION_MATRIZ && turno; j++){
             let oCount = 0;      
@@ -62,8 +64,10 @@ function turnoIA(i,j){
             if(oCount == 2 && xCount == 0){
                 vista[pos[0]][pos[1]].innerHTML = "O";
                 turno = false;
+                console.log("Ataque vertical")
             }
         }
+
         //Diagonales
         //Diagonal principal
         let oCount = 0;
@@ -82,7 +86,9 @@ function turnoIA(i,j){
         if(turno && xCount == 0 && oCount == 2){
             vista[pos[0]][pos[1]].innerHTML = "O";
             turno = false;
+            console.log("Ataque diagonal")
         }
+
         //Diagonal inversa
         if(turno){
             if(vista[0][2].innerHTML == "O" && vista[1][1].innerHTML == "O" && vista[2][0].innerHTML == ""){
@@ -94,14 +100,16 @@ function turnoIA(i,j){
             }else if(vista[2][0].innerHTML == "O" && vista[1][1].innerHTML == "O" && vista[0][2].innerHTML == ""){
                 vista[0][2].innerHTML = "O";
                 turno = false;
+                console.log("Ataque diagonal inversa")
             }
         }
     }
-    
+
     //MEJORA en caso de no tener coincidencias buscamos el centro del tablero
     if(turno && vista[1][1].innerHTML == ""){
         vista[1][1].innerHTML = "O";
         turno = false;
+        console.log("primera centro")
     }
 
     //Bloqueo horizontal
@@ -119,6 +127,7 @@ function turnoIA(i,j){
         if(oCount != 1 && oCount != 2 && pos.length == 1){
             vista[filas][pos].innerHTML = "O";
             turno = false;
+            console.log("defensa horizontal")
         }
     }
 
@@ -137,6 +146,7 @@ function turnoIA(i,j){
         if(oCount != 1 && oCount != 2 && pos.length == 1){
             vista[pos[0][0]][pos[0][1]].innerHTML = "O";
             turno = false;
+            console.log("defensa vertical")
         }
     }
 
@@ -158,6 +168,7 @@ function turnoIA(i,j){
         if(oCount == 0 && xCount == 2){
             vista[pos[0]][pos[0]].innerHTML = "O";
             turno = false;
+            console.log("diagonal defensa")
         }
     }
 
@@ -166,13 +177,16 @@ function turnoIA(i,j){
         if(vista[0][2].innerHTML == "X" && vista[1][1].innerHTML == "X" && vista[2][0].innerHTML == ""){
             vista[2][0].innerHTML = "O";
             turno = false;
+            console.log("diagonal inversa defensa")
         }else if(vista[0][2].innerHTML == "X" && vista[2][0].innerHTML == "X" && vista[1][1].innerHTML == ""){
             vista[1][1].innerHTML = "O";
             turno = false;
+            console.log("diagonal inversa defensa")
             console.log("defensa diagonal r");
         }else if(vista[2][0].innerHTML == "X" && vista[1][1].innerHTML == "X" && vista[0][2].innerHTML == ""){
             vista[0][2].innerHTML = "O";
             turno = false;
+            console.log("diagonal inversa defensa")
         }
     }
 
@@ -180,22 +194,38 @@ function turnoIA(i,j){
         let direccion;
         let error;
         let contorlBucle = 0;
+        console.log("esquinas controladas")
         do{
             error = false;
-            if(i == 0 && j == 1){
+            if(i == 0 && j == 0){
+                direccion = Math.floor(Math.random()*2)+1;
+            }else if(i == 0 && j == 2){
+                do{
+                    direccion = Math.floor(Math.random()*4);
+                }while(direccion == 1 || direccion == 2);
+            }else if(i == 2 && j == 0){
+                do{
+                    direccion = Math.floor(Math.random()*4);
+                }while(direccion == 2 || direccion == 1);
+            }else if(i == 2 && j == 2){
+                do{
+                    direccion = Math.floor(Math.random()*4);
+                }while(direccion == 3 || direccion == 0);
+            }else if(i == 0 && j == 1){
                 direccion = Math.floor(Math.random()*2);
             }else if(i == 1 && j == 0){
-                direccion = Math.floor(Math.random()*3);
-                while(direccion == 1){
+                do{
                     direccion = Math.floor(Math.random()*3);
-                }
+                }while(direccion == 1);
             }else if(i == 2 && j == 1){
                 direccion = 2 + Math.floor(Math.random()*2);
             }else if(i == 1 && j == 2){
-                direccion = Math.floor(Math.random()*4);
-                while(direccion == 0 || direccion == 2){
+                do{
                     direccion = Math.floor(Math.random()*4);
-                }
+                }while(direccion == 0 || direccion == 2);
+            }else{
+                error = true;
+                direccion = null;
             }
             /**
              * Direcciones permitidas
@@ -237,34 +267,81 @@ function turnoIA(i,j){
                         error = true;
                     }
                 break;
+                default:
+                    console.log("Error general");
+                    error = true;
             }
-            contorlBucle++;
-            if(contorlBucle>=10000){
-                error = false;
-            }
-        }while(error);
+        }while(!error && turno);
     }
     //Si no se ha colocado en alguna de las opciones anteriores se colocara aleatoriamente
     if(turno){
-        let contorlBucle = 0;
-        let filas = Math.floor(Math.random()*3);
-        let columnas = Math.floor(Math.random()*3);
+        console.log("esquinas aleatorio")
         let error = false;
-        while(!error && vista[filas][columnas].innerHTML != "" ){
-            filas = Math.floor(Math.random()*3);
-            columnas = Math.floor(Math.random()*3);
-            contorlBucle++;
-            if(contorlBucle>=10000){
+        let controlBucle = 0;
+        let notFound = false;
+        do{
+            let direccion = Math.floor(Math.random()*4);
+            switch(direccion){
+                case 0: 
+                    if(vista[0][0].innerHTML == ""){
+                        vista[0][0].innerHTML = "O";
+                        turno = false;
+                    }else{
+                        notFound = true;
+                    }
+                break;
+                case 1:
+                    if(vista[0][DIMENSION_MATRIZ-1].innerHTML == ""){
+                        vista[0][DIMENSION_MATRIZ-1].innerHTML = "O";
+                        turno = false;
+                    }else{
+                        notFound = true;
+                    }
+                break;
+                case 2:
+                    if(vista[DIMENSION_MATRIZ-1][0].innerHTML == ""){
+                        vista[DIMENSION_MATRIZ-1][0].innerHTML = "O";
+                        turno = false;
+                    }else{
+                        notFound = true;
+                    }
+                break;
+                case 3:
+                    if(vista[DIMENSION_MATRIZ-1][DIMENSION_MATRIZ-1].innerHTML == ""){
+                        vista[DIMENSION_MATRIZ-1][DIMENSION_MATRIZ-1].innerHTML = "O";
+                        turno = false;
+                    }else{
+                        notFound = true;
+                    }
+                break;
+                default:
+                    console.log("Error general");
+                    error = true;
+            }
+            if(notFound){
+                let filas;
+                let columnas;
+                do{
+                    filas = Math.floor(Math.random()*3);
+                    columnas = Math.floor(Math.random()*3);
+                    controlBucle++;
+                    if(controlBucle >= 10000){
+                        error = true;
+                    }
+                }while(!error && vista[filas][columnas].innerHTML != "");
+                if(!error){
+                    vista[filas][columnas].innerHTML = "O";
+                    turno = false;
+                }
+            }
+            controlBucle++;
+            if(controlBucle == 10000){
                 error = true;
             }
-        }
-        if(!error && vista[filas][columnas].innerHTML == ""){
-            vista[filas][columnas].innerHTML = "O";
-            turno = false;
-        }
-        turno = false;
+        }while(turno && !error);
     }
 }
+
 
 
 //Funcion para comprobar quien ha ganado
